46733a9d7e57a1a2e802fb54b2978beb15de6023
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2022-05-11 19:18:15 -0500

    Print steps

--- AutomataPila/Program.cs
+++ AutomataPila/Program.cs
@@ -40,108 +40,108 @@ var automaton = new PushdownAutomaton(new List<string>
     },
     new Dictionary<TransitionOrigin, TransitionDestination>
     {
-        { new ("q0", '1', null), new ("q1", new List<string> { "C" }) },
-        { new ("q0", '2', null), new ("q2", new List<string> { "J" }) },
-        { new ("q0", '3', null), new ("q3", new List<string> { "F" }) },
-        { new ("q0", '4', null), new ("q4", new List<string> { "T" }) },
-        { new ("q0", '5', null), new ("q5", new List<string> { "K" }) },
-        
-        { new ("q1", 'a', "C"), new ("q1", new List<string> { "C", "J" }) },
-        { new ("q1", 'b', "C"), new ("q3", new List<string> { "F", "C" }) },
-        { new ("q1", 'c', "C"), new ("q1", new List<string> { "C", "T" }) },
-        { new ("q1", 'A', "C"), new ("q6", null) },
-        { new ("q1", 'B', "C"), new ("q6", null) },
-        { new ("q1", 'C', "C"), new ("q6", null) },
-        { new ("q1", 'D', "C"), new ("q6", null) },
-        { new ("q1", 'E', "C"), new ("q6", null) },
-        { new ("q1", 'F', "C"), new ("q6", null) },
-        
-        { new ("q2", 'a', "J"), new ("q2", new List<string> { "J", "J" }) },
-        { new ("q2", 'b', "J"), new ("q2", new List<string> { "J", "F" }) },
-        { new ("q2", 'c', "J"), new ("q3", new List<string> { "F", "C" }) },
-        { new ("q2", 'A', "J"), new ("q6", null) },
-        { new ("q2", 'B', "J"), new ("q6", null) },
-        { new ("q2", 'C', "J"), new ("q6", null) },
-        { new ("q2", 'D', "J"), new ("q6", null) },
-        { new ("q2", 'E', "J"), new ("q6", null) },
-        { new ("q2", 'F', "J"), new ("q6", null) },
-        
-        { new ("q3", 'a', "F"), new ("q3", new List<string> { "F", "C" }) },
-        { new ("q3", 'b', "F"), new ("q3", new List<string> { "F", "J" }) },
-        { new ("q3", 'c', "F"), new ("q3", new List<string> { "F", "K" }) },
-        { new ("q3", 'A', "F"), new ("q6", null) },
-        { new ("q3", 'B', "F"), new ("q6", null) },
-        { new ("q3", 'C', "F"), new ("q6", null) },
-        { new ("q3", 'D', "F"), new ("q6", null) },
-        { new ("q3", 'E', "F"), new ("q6", null) },
-        { new ("q3", 'F', "F"), new ("q6", null) },
-        
-        { new ("q4", 'a', "T"), new ("q1", new List<string> { "C", "T" }) },
-        { new ("q4", 'b', "T"), new ("q3", new List<string> { "F", "T" }) },
-        { new ("q4", 'c', "T"), new ("q4", new List<string> { "T" }) },
-        { new ("q4", 'A', "T"), new ("q6", null) },
-        { new ("q4", 'B', "T"), new ("q6", null) },
-        { new ("q4", 'C', "T"), new ("q6", null) },
-        { new ("q4", 'D', "T"), new ("q6", null) },
-        { new ("q4", 'E', "T"), new ("q6", null) },
-        { new ("q4", 'F', "T"), new ("q6", null) },
-        
-        { new ("q5", 'a', "K"), new ("q1", new List<string> { "C", "K" }) },
-        { new ("q5", 'b', "K"), new ("q5", new List<string> { "K", "T" }) },
-        { new ("q5", 'c', "K"), new ("q3", new List<string> { "F", "K" }) },
-        { new ("q5", 'A', "K"), new ("q6", null) },
-        { new ("q5", 'B', "K"), new ("q6", null) },
-        { new ("q5", 'C', "K"), new ("q6", null) },
-        { new ("q5", 'D', "K"), new ("q6", null) },
-        { new ("q5", 'E', "K"), new ("q6", null) },
-        { new ("q5", 'F', "K"), new ("q6", null) },
-        
-        { new ("q6", 'a', "C"), new ("q1", new List<string> { "C", "J" }) },
-        { new ("q6", 'b', "C"), new ("q3", new List<string> { "F", "C" }) },
-        { new ("q6", 'c', "C"), new ("q1", new List<string> { "C", "T" }) },
-        { new ("q6", 'a', "J"), new ("q2", new List<string> { "J", "J" }) },
-        { new ("q6", 'b', "J"), new ("q2", new List<string> { "J", "F" }) },
-        { new ("q6", 'c', "J"), new ("q3", new List<string> { "F", "C" }) },
-        { new ("q6", 'a', "F"), new ("q3", new List<string> { "F", "C" }) },
-        { new ("q6", 'b', "F"), new ("q3", new List<string> { "F", "J" }) },
-        { new ("q6", 'c', "F"), new ("q3", new List<string> { "F", "K" }) },
-        { new ("q6", 'a', "T"), new ("q1", new List<string> { "C", "T" }) },
-        { new ("q6", 'b', "T"), new ("q3", new List<string> { "F", "T" }) },
-        { new ("q6", 'c', "T"), new ("q4", new List<string> { "T" }) },
-        { new ("q6", 'a', "K"), new ("q1", new List<string> { "C", "K" }) },
-        { new ("q6", 'b', "K"), new ("q5", new List<string> { "K", "T" }) },
-        { new ("q6", 'c', "K"), new ("q3", new List<string> { "F", "K" }) },
-        { new ("q6", 'A', "C"), new ("q6", null) },
-        { new ("q6", 'B', "C"), new ("q6", null) },
-        { new ("q6", 'C', "C"), new ("q6", null) },
-        { new ("q6", 'D', "C"), new ("q6", null) },
-        { new ("q6", 'E', "C"), new ("q6", null) },
-        { new ("q6", 'F', "C"), new ("q6", null) },
-        { new ("q6", 'A', "F"), new ("q6", null) },
-        { new ("q6", 'B', "F"), new ("q6", null) },
-        { new ("q6", 'C', "F"), new ("q6", null) },
-        { new ("q6", 'D', "F"), new ("q6", null) },
-        { new ("q6", 'E', "F"), new ("q6", null) },
-        { new ("q6", 'F', "F"), new ("q6", null) },
-        { new ("q6", 'A', "J"), new ("q6", null) },
-        { new ("q6", 'B', "J"), new ("q6", null) },
-        { new ("q6", 'C', "J"), new ("q6", null) },
-        { new ("q6", 'D', "J"), new ("q6", null) },
-        { new ("q6", 'E', "J"), new ("q6", null) },
-        { new ("q6", 'F', "J"), new ("q6", null) },
-        { new ("q6", 'A', "T"), new ("q6", null) },
-        { new ("q6", 'B', "T"), new ("q6", null) },
-        { new ("q6", 'C', "T"), new ("q6", null) },
-        { new ("q6", 'D', "T"), new ("q6", null) },
-        { new ("q6", 'E', "T"), new ("q6", null) },
-        { new ("q6", 'F', "T"), new ("q6", null) },
-        { new ("q6", 'A', "K"), new ("q6", null) },
-        { new ("q6", 'B', "K"), new ("q6", null) },
-        { new ("q6", 'C', "K"), new ("q6", null) },
-        { new ("q6", 'D', "K"), new ("q6", null) },
-        { new ("q6", 'E', "K"), new ("q6", null) },
-        { new ("q6", 'F', "K"), new ("q6", null) },
-        { new ("q6", '|', "@"), new ("q7", null) },
+        { new("q0", '1', null), new("q1", new List<string> { "C" }) },
+        { new("q0", '2', null), new("q2", new List<string> { "J" }) },
+        { new("q0", '3', null), new("q3", new List<string> { "F" }) },
+        { new("q0", '4', null), new("q4", new List<string> { "T" }) },
+        { new("q0", '5', null), new("q5", new List<string> { "K" }) },
+
+        { new("q1", 'a', "C"), new("q1", new List<string> { "C", "J" }) },
+        { new("q1", 'b', "C"), new("q3", new List<string> { "F", "C" }) },
+        { new("q1", 'c', "C"), new("q1", new List<string> { "C", "T" }) },
+        { new("q1", 'A', "C"), new("q6", null) },
+        { new("q1", 'B', "C"), new("q6", null) },
+        { new("q1", 'C', "C"), new("q6", null) },
+        { new("q1", 'D', "C"), new("q6", null) },
+        { new("q1", 'E', "C"), new("q6", null) },
+        { new("q1", 'F', "C"), new("q6", null) },
+
+        { new("q2", 'a', "J"), new("q2", new List<string> { "J", "J" }) },
+        { new("q2", 'b', "J"), new("q2", new List<string> { "J", "F" }) },
+        { new("q2", 'c', "J"), new("q3", new List<string> { "F", "C" }) },
+        { new("q2", 'A', "J"), new("q6", null) },
+        { new("q2", 'B', "J"), new("q6", null) },
+        { new("q2", 'C', "J"), new("q6", null) },
+        { new("q2", 'D', "J"), new("q6", null) },
+        { new("q2", 'E', "J"), new("q6", null) },
+        { new("q2", 'F', "J"), new("q6", null) },
+
+        { new("q3", 'a', "F"), new("q3", new List<string> { "F", "C" }) },
+        { new("q3", 'b', "F"), new("q3", new List<string> { "F", "J" }) },
+        { new("q3", 'c', "F"), new("q3", new List<string> { "F", "K" }) },
+        { new("q3", 'A', "F"), new("q6", null) },
+        { new("q3", 'B', "F"), new("q6", null) },
+        { new("q3", 'C', "F"), new("q6", null) },
+        { new("q3", 'D', "F"), new("q6", null) },
+        { new("q3", 'E', "F"), new("q6", null) },
+        { new("q3", 'F', "F"), new("q6", null) },
+
+        { new("q4", 'a', "T"), new("q1", new List<string> { "C", "T" }) },
+        { new("q4", 'b', "T"), new("q3", new List<string> { "F", "T" }) },
+        { new("q4", 'c', "T"), new("q4", new List<string> { "T" }) },
+        { new("q4", 'A', "T"), new("q6", null) },
+        { new("q4", 'B', "T"), new("q6", null) },
+        { new("q4", 'C', "T"), new("q6", null) },
+        { new("q4", 'D', "T"), new("q6", null) },
+        { new("q4", 'E', "T"), new("q6", null) },
+        { new("q4", 'F', "T"), new("q6", null) },
+
+        { new("q5", 'a', "K"), new("q1", new List<string> { "C", "K" }) },
+        { new("q5", 'b', "K"), new("q5", new List<string> { "K", "T" }) },
+        { new("q5", 'c', "K"), new("q3", new List<string> { "F", "K" }) },
+        { new("q5", 'A', "K"), new("q6", null) },
+        { new("q5", 'B', "K"), new("q6", null) },
+        { new("q5", 'C', "K"), new("q6", null) },
+        { new("q5", 'D', "K"), new("q6", null) },
+        { new("q5", 'E', "K"), new("q6", null) },
+        { new("q5", 'F', "K"), new("q6", null) },
+
+        { new("q6", 'a', "C"), new("q1", new List<string> { "C", "J" }) },
+        { new("q6", 'b', "C"), new("q3", new List<string> { "F", "C" }) },
+        { new("q6", 'c', "C"), new("q1", new List<string> { "C", "T" }) },
+        { new("q6", 'a', "J"), new("q2", new List<string> { "J", "J" }) },
+        { new("q6", 'b', "J"), new("q2", new List<string> { "J", "F" }) },
+        { new("q6", 'c', "J"), new("q3", new List<string> { "F", "C" }) },
+        { new("q6", 'a', "F"), new("q3", new List<string> { "F", "C" }) },
+        { new("q6", 'b', "F"), new("q3", new List<string> { "F", "J" }) },
+        { new("q6", 'c', "F"), new("q3", new List<string> { "F", "K" }) },
+        { new("q6", 'a', "T"), new("q1", new List<string> { "C", "T" }) },
+        { new("q6", 'b', "T"), new("q3", new List<string> { "F", "T" }) },
+        { new("q6", 'c', "T"), new("q4", new List<string> { "T" }) },
+        { new("q6", 'a', "K"), new("q1", new List<string> { "C", "K" }) },
+        { new("q6", 'b', "K"), new("q5", new List<string> { "K", "T" }) },
+        { new("q6", 'c', "K"), new("q3", new List<string> { "F", "K" }) },
+        { new("q6", 'A', "C"), new("q6", null) },
+        { new("q6", 'B', "C"), new("q6", null) },
+        { new("q6", 'C', "C"), new("q6", null) },
+        { new("q6", 'D', "C"), new("q6", null) },
+        { new("q6", 'E', "C"), new("q6", null) },
+        { new("q6", 'F', "C"), new("q6", null) },
+        { new("q6", 'A', "F"), new("q6", null) },
+        { new("q6", 'B', "F"), new("q6", null) },
+        { new("q6", 'C', "F"), new("q6", null) },
+        { new("q6", 'D', "F"), new("q6", null) },
+        { new("q6", 'E', "F"), new("q6", null) },
+        { new("q6", 'F', "F"), new("q6", null) },
+        { new("q6", 'A', "J"), new("q6", null) },
+        { new("q6", 'B', "J"), new("q6", null) },
+        { new("q6", 'C', "J"), new("q6", null) },
+        { new("q6", 'D', "J"), new("q6", null) },
+        { new("q6", 'E', "J"), new("q6", null) },
+        { new("q6", 'F', "J"), new("q6", null) },
+        { new("q6", 'A', "T"), new("q6", null) },
+        { new("q6", 'B', "T"), new("q6", null) },
+        { new("q6", 'C', "T"), new("q6", null) },
+        { new("q6", 'D', "T"), new("q6", null) },
+        { new("q6", 'E', "T"), new("q6", null) },
+        { new("q6", 'F', "T"), new("q6", null) },
+        { new("q6", 'A', "K"), new("q6", null) },
+        { new("q6", 'B', "K"), new("q6", null) },
+        { new("q6", 'C', "K"), new("q6", null) },
+        { new("q6", 'D', "K"), new("q6", null) },
+        { new("q6", 'E', "K"), new("q6", null) },
+        { new("q6", 'F', "K"), new("q6", null) },
+        { new("q6", '|', "@"), new("q7", null) },
     },
     "q0",
     "@",
@@ -151,9 +151,8 @@ var automaton = new PushdownAutomaton(new List<string>
     });
 do
 {
-
     Console.WriteLine("Ingresa la palabra a comprobar");
-    Console.WriteLine(automaton.ValidForInput(Console.ReadLine()));
+    Console.WriteLine(automaton.ValidForInput(Console.ReadLine()) ? "La palabra si es aceptada por el automata" : "La palabra no es aceptada por el automata");
 } while (true);
 
 public class PushdownAutomaton
@@ -219,6 +218,14 @@ public class PushdownAutomaton
         this.acceptingStates = new List<string>(acceptingStates);
     }
 
+    private void printCurrentState(string remainingInput)
+    {
+        string[] bak = new string[stack.Count];
+        stack.CopyTo(bak, 0);
+
+        Console.WriteLine($"({currentState}, {(remainingInput.Length == 0 ? "λ" : remainingInput)}, {(bak.Length == 0 ? "λ" : string.Concat(bak))})");
+    }
+
     public bool ValidForInput(string input)
     {
         if (!input.All(c => inputAlphabet.Contains(c)))
@@ -233,11 +240,16 @@ public class PushdownAutomaton
 
         currentState = initialState;
 
+        printCurrentState(modifiable);
+
         int consumed;
 
         while ((consumed = AdvanceWithInput(modifiable.Length == 0 ? null : modifiable[0])) >= 0)
         {
             modifiable = modifiable[consumed..];
+
+            Console.Write('├');
+            printCurrentState(modifiable);
 
             if (modifiable.Length == 0 && acceptingStates.Contains(currentState))
                 return true;
@@ -247,15 +259,14 @@ public class PushdownAutomaton
     }
 
     private int AdvanceWithInput(char? input)
-    
     {
         for (int i = input == null ? 2 : 0; i < 4; i++)
         {
             var origin = new TransitionOrigin(currentState, i < 2 ? input : null, i % 2 == 0 ? StackTop : null);
 
-            if (!transitionRelation.TryGetValue(origin, out var destination)) 
+            if (!transitionRelation.TryGetValue(origin, out var destination))
                 continue;
-            
+
             currentState = destination.State;
 
             if (i % 2 == 0 && StackTop != null)
